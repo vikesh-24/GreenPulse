e0f638202080458ed80c66bafdf79740a01487f4
Brinthapan <brinthapanj@gmail.com>
2025-03-22 17:18:16 +0530

    frontend issue fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home'; // Create a Home component for the root path
 import ProjectList from './components/projectManagement/ProjectList';
-import AddProject from './components/projectManagement/AddProject';
-import EditProject from './components/projectManagement/EditProject';
-import DeleteProject from './components/projectManagement/DeleteProject';
+
+
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Register from './components/usermanagement/Register';
@@ -13,7 +13,7 @@ import About from './components/about';
 import AddDonor from './components/DonorManagement/AddDonor';
 import MockPurchase from './components/DonorManagement/MockPurchase';
 import Donors from './components/DonorManagement/Donors';
-import AdminDashboard from './components/Admin/Admindashboard';
+
 import Profile from './components/usermanagement/Profile';
 
 const App = () => {
@@ -30,9 +30,7 @@ const App = () => {
 
                     {/* Project management routes */}
                     <Route path="/projectManagement/list" element={<ProjectList />} />
-                    <Route path="/projectManagement/add" element={<AddProject />} />
-                    <Route path="/projectManagement/edit/:id" element={<EditProject />} />
-                    <Route path="/projectManagement/delete/:id" element={<DeleteProject />} />
+                   
 
                     {/* User routes */}
                     <Route path="/register" element={<Register />} />
@@ -48,7 +46,7 @@ const App = () => {
                     <Route path="/donors" element={<Donors />} />
 
                     {/* Admin dashboard */}
-                    <Route path="/admin" element={<AdminDashboard />} />
+                   
                 </Routes>
 
                 {/* Footer is displayed on all pages */}
